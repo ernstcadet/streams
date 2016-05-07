1eb89c15d6bd9d0c88a83667947e5c460e1615b8
ernstcadet <cadetannlou@gmail.com>
2016-05-07 17:42:58 +0000

    restarting

--- routes.js
+++ routes.js
@@ -1,20 +1,21 @@
-//Local deps
+// Local dependencies
 var controllers = require('./controllers');
 
 var Router = {
-  
-  init: function(app) {
- 
-   // Home page
-   app.get('/', controllers.Home.index);
-  
-  // Users 
-  app.get('/users', controllers.Users.index);
-  
-  
-  
- }
-    
+
+    init: function(app) {
+
+        // Home page
+        app.get('/', controllers.Home.index);
+
+        // About page
+        app.get('/about', controllers.About.index);
+
+        // Users
+        app.get('/users', controllers.Users.index);
+
+    }
+
 };
 
 module.exports = Router;
